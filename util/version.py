44270f0a378e79e42a23f890c15287f61e78ab42
Colin Heslin <cheslin23t@gmail.com>
2025-12-31 22:25:24 -0500

    Version update

--- util/version.py
+++ util/version.py
@@ -1,3 +1,3 @@
-__version__ = "0.9.4"
+__version__ = "0.9.5"
 __production__ = False # Used to indicate if this is a production build. Can be changed post-build to subscribe to a different channel during update checks.
 # These don't go into .env as GitHub Actions needs to be able to access them.
