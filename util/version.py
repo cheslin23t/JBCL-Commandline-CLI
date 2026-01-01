1178373190f9a9a9591abc02e5b2e62e62ae2fcb
Colin Heslin <cheslin23t@gmail.com>
2025-12-31 21:40:46 -0500

    Test Production x2

--- util/version.py
+++ util/version.py
@@ -1,3 +1,3 @@
-__version__ = "0.9.3 TEST PRODUCTION"
+__version__ = "0.9.3-TEST-PRODUCTION"
 __production__ = True # Used to indicate if this is a production build. Can be changed post-build to subscribe to a different channel during update checks.
 # These don't go into .env as GitHub Actions needs to be able to access them.
