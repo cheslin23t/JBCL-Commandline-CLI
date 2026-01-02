cace1adc37b5802cb6dec4ba5b26483a292e9b2b
Colin Heslin <67148623+cheslin23t@users.noreply.github.com>
2026-01-02 01:29:35 -0500

    Bump version to 1.0.1 and set production to False
    Updated version and production status for the build.

--- util/version.py
+++ util/version.py
@@ -1,3 +1,3 @@
-__version__ = "1.0.0"
-__production__ = True # Used to indicate if this is a production build. Can be changed post-build to subscribe to a different channel during update checks.
+__version__ = "1.0.1"
+__production__ = False # Used to indicate if this is a production build. Can be changed post-build to subscribe to a different channel during update checks.
 # These don't go into .env as GitHub Actions needs to be able to access them.
